925f1faa3beeea1aec96455579e5175f5fffa7b4
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-04 22:35:25 -0500

    Initial commit for 'TDD First Program' assignment.

---
+++ camper_age_input_hofmann/camper_age_input_hofmann.py
@@ -0,0 +1,16 @@
+"""
+Program: camper_age_input_hofmann.py
+Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+Last Modified: 6/3/2020
+
+Program specifications: The program will have will prompt for the age of one infant (age 1-5 years)
+who is attending camp and convert the age in months to years via a function call then print the result.
+"""
+
+
+def convert_to_months(x):
+    pass
+
+
+if __name__ == '__main__':
+    pass
